18334287eb75a0ad49cc552c9dd9de27d9f341f8
Yuan Ru Sheng <1978820363@qq.com/n>
2024-09-04 02:50:34 +0800

    修改菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,11 +1,23 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface DishFlavorMapper {
     void insertBatch(List<DishFlavor> flavors);
+
+    void deleteByDishIds(List<Long> dishIds);
+
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishIds(Long dishId);
+
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long id);
+
+    
 }
